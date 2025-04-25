6bcb2cdcf000eeb191253044d9eac1584e4b8876
Nayara Moises <nayaramoises7@gmail.com>
2025-04-25 02:07:13 -0300

    primeiro commit do projeto conversor de temperatura

---
+++ ConversorTemperatura.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ConversorTemperatura {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Escolha o tipo de conversão:");
+        System.out.println("1 - Celsius para Fahrenheit");
+        System.out.println("2 - Fahrenheit para Celsius");
+        System.out.print("Digite a opção: ");
+        
+        int opcao = scanner.nextInt();
+        
+        if (opcao == 1) {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double celsius = scanner.nextDouble();
+            double fahrenheit = (celsius * 9/5) + 32;
+            System.out.println(celsius + " graus Celsius é igual a " + fahrenheit + " graus Fahrenheit.");
+        } else if (opcao == 2) {
+            System.out.print("Digite a temperatura em Fahrenheit: ");
+            double fahrenheit = scanner.nextDouble();
+            double celsius = (fahrenheit - 32) * 5/9;
+            System.out.println(fahrenheit + " graus Fahrenheit é igual a " + celsius + " graus Celsius.");
+        } else {
+            System.out.println("Opção inválida.");
+        }
+
+        scanner.close();
+    }
+}
